dc000b9b238eff790d257ff2f0cbc27df543d5a3
Yaroslav <solomatin.yarik@mail.ru>
2025-02-28 18:38:11 +0300

    after 5 task (added logic for brain-even)

--- src/cli.js
+++ src/cli.js
@@ -2,8 +2,7 @@ import { question } from "readline-sync";
 
 export const sayHello = () => {
     console.log("Welcome to the Brain Games!");
-    const userName = question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
-
-    return;
+    const name = question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+    return name;
 }
---
+++ src/games/brain-even-logic.js
@@ -0,0 +1,41 @@
+import { userName } from "../../bin/brain-games.js";
+import { question } from "readline-sync";
+
+const isRight = (userAnswer, number) => {
+    if (userAnswer === 'yes' | userAnswer === 'no') {
+        if (userAnswer === 'yes' && number % 2 === 0) {
+            return true;
+        } else if (userAnswer === 'no' && number % 2 !== 0) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+export const brainEven = () => {
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+    let isCorrect = true;
+    let i = 0;
+
+    while (isCorrect) {
+        const number = Math.floor(Math.random() * 100);
+        const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+        console.log(`Question: ${number}`);
+
+        const userAnswer = question('Your answer: ');
+
+        if (isRight(userAnswer, number)) {
+            console.log('Correct!');
+            i++;
+            if (i === 3) {
+                console.log(`Congratulations, ${userName}`);
+                break;
+            }
+        } else {
+            isCorrect = false;
+            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'\nLet's try again, ${userName}`);
+        }
+    }
+}
