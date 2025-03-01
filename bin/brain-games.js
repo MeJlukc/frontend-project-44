6885628bd513b8fa726d410350cb27ef9a0c3c6d
Yaroslav <solomatin.yarik@mail.ru>
2025-03-01 05:46:24 +0300

    updated cli.js and brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import { sayHello } from '../src/cli.js';
 
-export const userName = sayHello();
+sayHello();
--- src/cli.js
+++ src/cli.js
@@ -4,5 +4,4 @@ export const sayHello = () => {
     console.log("Welcome to the Brain Games!");
     const name = question('May I have your name? ');
     console.log(`Hello, ${name}!`);
-    return name;
 }
