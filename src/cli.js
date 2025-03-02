cd0f12ebc1f68b6e598eb6c877964982fb00784b
Yaroslav <solomatin.yarik@mail.ru>
2025-03-03 00:15:36 +0300

    fixed cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,7 @@
 import { question } from 'readline-sync';
 
-const sayHello = () => {
+export const sayHello = () => {
     console.log('Welcome to the Brain Games!');
     const name = question('May I have your name? ');
     console.log(`Hello, ${name}!`);
 };
-
-export default sayHello;
